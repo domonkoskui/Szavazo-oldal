08795ad3c1756fed061e38e7ccb6d7d63716a1ca
szt07 <szt07@kamuemail.hu>
2021-05-31 13:03:31 +0200

    choose your fighter(Tanár)

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ let app= express();
 /*app.get("/",function(req,res){
     res.end("Hello World")
 })*/
-app.listen(9000);
+app.use(express.urlencoded());
+app.post("/szavazas", function(req, res){
+    console.log(req.body)
+})
+
 
 app.use(express.static('public'));
-
-
-
+app.listen(9000);
