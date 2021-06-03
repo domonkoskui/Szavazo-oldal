ddb83acfc1f98ddef24b29399c993d8da3717e78
szt07 <szt07@kamuemail.hu>
2021-06-03 11:43:43 +0200

    Ez már mongoban is adatot hoppácska

--- server.js
+++ server.js
@@ -1,11 +1,46 @@
 let express = require("express");
+let mongoose = require('mongoose');
+let config= require('./config.json');
+
+mongoose.connect(config.databaseURL);
+
 let app= express();
 /*app.get("/",function(req,res){
     res.end("Hello World")
 })*/
-app.use(express.urlencoded());
+
+const schema = new mongoose.Schema({
+    nev: String,
+    szavazatok : Number
+});
+
+const model =  mongoose.model('Opciok',schema , 'Opciok');
+
+app.use(express.urlencoded({
+    extended: true
+}));
 app.post("/szavazas", function(req, res){
-    console.log(req.body)
+    console.log(req.body);
+
+    model.findOne({ nev: req.body.tanár}, function(err, doc){
+        if(doc)
+        {
+            console.log(req.body.tanár + ' már létezik')
+            doc.szavazatok++;
+            doc.save();
+        }
+        else{
+            console.log(req.body.tanár+' még nem létezik')
+        new model({
+            
+            nev: req.body.tanár,
+            szavazatok : 1
+        }).save();
+        }
+    });
+    
+  
+    res.redirect('/');
 })
 
 
